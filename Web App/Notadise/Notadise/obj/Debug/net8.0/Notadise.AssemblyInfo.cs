d48f24980eaa34eccc4f36d5f29c5e1f0a6a8154
Geri <85336812+GIBineva@users.noreply.github.com>
2024-12-05 14:28:41 +0200

    FIxed categories sorting

--- Web App/Notadise/Notadise/obj/Debug/net8.0/Notadise.AssemblyInfo.cs
+++ Web App/Notadise/Notadise/obj/Debug/net8.0/Notadise.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notadise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f35859045cee3575598bf447550b6829b3d8e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472a13a35958a6fa2dd4c717e6fedb514f680907")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notadise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notadise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
